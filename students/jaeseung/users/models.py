34054a0aba7c516e4fea87813bf2921fda2c6521
jaeseung <jhwang90801@gmail.com>
2022-05-12 16:16:18 +0900

    3rd modified - project modeling

--- students/jaeseung/users/models.py
+++ students/jaeseung/users/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class User(models.Model): 
-    name        = models.CharField(max_length=45, unique=True)
-    email       = models.EmailField(max_length=100, unique=True)
+    name        = models.CharField(max_length=45)
+    email       = models.CharField(max_length=100, unique=True)
     password    = models.CharField(max_length=128)
     contact     = models.CharField(max_length=45, unique=True)
     created_at  = models.DateTimeField(auto_now_add=True)
