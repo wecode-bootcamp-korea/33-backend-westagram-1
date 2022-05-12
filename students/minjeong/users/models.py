0651bd8a0c49353ca6109cf75aa57268abd25894
minjeong kim <ebsgreat7@gmail.com>
2022-05-12 16:31:13 +0900

    데이터 이력관리를 위한 모델 필드 추가2

--- students/minjeong/users/models.py
+++ students/minjeong/users/models.py
@@ -7,8 +7,8 @@ class User(models.Model):
     password    = models.CharField(max_length=100)
     phonenumber = models.CharField(max_length=45, blank=True)
     personal    = models.CharField(max_length=45, blank=True)
-    created_at  = models.DateTimeField(auto_now_add=True)
-    updated_at  = models.DateTimeField(auto_now_add=True)
+    created_at  = models.DateTimeField(auto_now=True)
+    updated_at  = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table='user'
