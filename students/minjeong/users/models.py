962b3b1f04d785a4cc226db58eb3245be92f6f84
minjeong kim <ebsgreat7@gmail.com>
2022-05-12 14:54:16 +0900

    유저모델작성

---
+++ students/minjeong/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Signup(models.Model):
+    name=models.CharField(max_length=45)
+    email=models.EmailField(max_length=45, unique=True)
+    password=models.CharField(max_length=45)
+    phonenumber=models.CharField(max_length=45, blank=True)
+    personal=models.CharField(max_length=45, blank=True)
+
+    class Meta:
+        db_table='signup'
