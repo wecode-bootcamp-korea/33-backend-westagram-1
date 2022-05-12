1d79e34555bb988c853db239cc92467d1bee6688
minjeong kim <ebsgreat7@gmail.com>
2022-05-12 15:55:39 +0900

    모델 수정

--- students/minjeong/users/models.py
+++ students/minjeong/users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-# Create your models here.
-class Signup(models.Model):
-    name=models.CharField(max_length=45)
-    email=models.EmailField(max_length=45, unique=True)
-    password=models.CharField(max_length=45)
-    phonenumber=models.CharField(max_length=45, blank=True)
-    personal=models.CharField(max_length=45, blank=True)
+
+class User(models.Model):
+    name        = models.CharField(max_length=45)
+    email       = models.CharField(max_length=45, unique=True)
+    password    = models.CharField(max_length=100)
+    phonenumber = models.CharField(max_length=45, blank=True)
+    personal    = models.CharField(max_length=45, blank=True)
 
     class Meta:
-        db_table='signup'
+        db_table='user'
