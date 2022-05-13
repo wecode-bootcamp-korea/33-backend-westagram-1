3f59308d23f8d561d9e6a402402bb3f9e35438fe
strzero <restouri@gmail.com>
2022-05-13 16:46:58 +0900

    users view 수정

--- students/seyoung/users/views.py
+++ students/seyoung/users/views.py
@@ -21,23 +21,22 @@ class UserSignUpView(View):
             input_mobile    = input_data["mobile_number"]
             input_birthdate = input_data["date_of_birth"]
                 
-            if re.match(REGEX_EMAIL, input_email) is None or User.objects.filter(email=input_email).exists():
+            if re.match(REGEX_EMAIL, input_email) == None or User.objects.filter(email=input_email).exists():
                 return JsonResponse({"message": "INVALID_EMAIL"}, status=400)
 
-            if re.match(REGEX_PASSWORD, input_password) is None:
+            if re.match(REGEX_PASSWORD, input_password) == None:
                 return JsonResponse({"message": "INVALID_PASSWORD"}, status=400)
 
-            if re.match(REGEX_MOBILE, input_mobile) is None:
+            if re.match(REGEX_MOBILE, input_mobile) == None:
                 return JsonResponse({"message": "INVALID_MOBILE_NUMNBER"}, status=400)
 
-            if re.match(REGEX_BIRTHDATE, input_birthdate) is None:
+            if re.match(REGEX_BIRTHDATE, input_birthdate) == None:
                 return JsonResponse({"message": "INVALID_DATE_OF_BIRTH"}, status=400)
 
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
-        else:
-            user = User.objects.create(
+        user = User.objects.create(
                 name          = input_name,
                 email         = input_email,
                 password      = input_password,
@@ -45,4 +44,4 @@ class UserSignUpView(View):
                 date_of_birth = input_birthdate,
             )
 
-            return JsonResponse({"message": "SUCCESS"}, status=201)
+        return JsonResponse({"message": "SUCCESS"}, status=201)
