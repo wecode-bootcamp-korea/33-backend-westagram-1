0c6a6e0d715f20f98bfee23a8f379400896ea5ec
strzero <restouri@gmail.com>
2022-05-13 16:50:19 +0900

    users view == 수정

--- students/seyoung/users/views.py
+++ students/seyoung/users/views.py
@@ -21,16 +21,16 @@ class UserSignUpView(View):
             input_mobile    = input_data["mobile_number"]
             input_birthdate = input_data["date_of_birth"]
                 
-            if re.match(REGEX_EMAIL, input_email) == None or User.objects.filter(email=input_email).exists():
+            if not re.match(REGEX_EMAIL, input_email) or User.objects.filter(email=input_email).exists():
                 return JsonResponse({"message": "INVALID_EMAIL"}, status=400)
 
-            if re.match(REGEX_PASSWORD, input_password) == None:
+            if not re.match(REGEX_PASSWORD, input_password):
                 return JsonResponse({"message": "INVALID_PASSWORD"}, status=400)
 
-            if re.match(REGEX_MOBILE, input_mobile) == None:
+            if not re.match(REGEX_MOBILE, input_mobile):
                 return JsonResponse({"message": "INVALID_MOBILE_NUMNBER"}, status=400)
 
-            if re.match(REGEX_BIRTHDATE, input_birthdate) == None:
+            if not re.match(REGEX_BIRTHDATE, input_birthdate):
                 return JsonResponse({"message": "INVALID_DATE_OF_BIRTH"}, status=400)
 
         except KeyError:
