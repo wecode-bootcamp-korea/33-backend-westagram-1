cfbb6c852ff2df6312a8c5ad58e660575bcf3a6b
jaeseung <jhwang90801@gmail.com>
2022-05-13 10:49:55 +0900

    CREATE: project SignUpView

--- students/jaeseung/users/views.py
+++ students/jaeseung/users/views.py
@@ -1,22 +1,46 @@
 import json
+import re
 
 from django.http import JsonResponse
 from django.views import View
 
 from users.models import User
 
-class UsersView(View):
+class SignUpView(View):
     def post(self, request):
-        data = json.loads(request.body)
-
-
-
-        User.objects.create(
-            name = data['name'],
-            email = data['email'],
+        try:
+            data = json.loads(request.body)
+            
+            name     = data['name'],
+            email    = data['email'],
             password = data['password'],
-            contact = data['contact']
-        )
+            contact  = data['contact']
 
-        except KeyError:
+            #Regex of email & password
+            email_regex    = "^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+            password_regex = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@!%*#?&])[A-Za-z\d@!%*#?&]{8,}$"
+            
+            #Email Validation
+            if re.match(email_regex, str(email)) == None:
+                return JsonResponse({"message": "INVALID_EMAIL"}, status=400)
+            
+            #Password Validation
+            if re.match(password_regex, str(password)) == None:
+                return JsonResponse({"message": "INVALID_PASSWORD"}, status=400)
+            
+            #Case(Email already exists)
+            if User.objects.filter(email = email).exists(): 
+                return JsonResponse({"message": "EMAIL_IS_ALREADY_REGISTERED"})
+
+            #Insert Data
+            User.objects.create(
+                name     = name,
+                email    = email,
+                password = password,
+                contact  = contact
+            )
+            
+            return JsonResponse({"message": "SUCCESS"}, status=201)
+
+        except KeyError: 
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
--- students/jaeseung/westagram/urls.py
+++ students/jaeseung/westagram/urls.py
@@ -1,6 +1,7 @@
 
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-
+    # users
+    path('users', include('users.urls'))
 ]
