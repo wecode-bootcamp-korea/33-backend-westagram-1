4439236ea947a4c127faea6cf553000ffd9ca8cf
jaeseung <jhwang90801@gmail.com>
2022-05-13 16:22:56 +0900

    2nd modified - project SignUpView

--- students/jaeseung/users/views.py
+++ students/jaeseung/users/views.py
@@ -11,25 +11,25 @@ class SignUpView(View):
         try:
             data = json.loads(request.body)
             
-            name     = data['name'],
-            email    = data['email'],
-            password = data['password'],
+            name     = data['name']
+            email    = data['email']
+            password = data['password']
             contact  = data['contact']
 
-            #Regex of email & password
+            # #Regex of email & password
             email_regex    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
             password_regex = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[@!%*#?&])[A-Za-z\d@!%*#?&]{8,}$'
             
-            #Email Validation
-            if re.match(email_regex, str(email)) == None:
+            # #Email Validation
+            if not re.match(email_regex, email):
                 return JsonResponse({"message": "INVALID_EMAIL"}, status=400)
-            
-            #Password Validation
-            if re.match(password_regex, str(password)) == None:
+
+            # #Password Validation
+            if not re.match(password_regex, password):
                 return JsonResponse({"message": "INVALID_PASSWORD"}, status=400)
             
             #Case(Email already exists)
-            if User.objects.filter(email = email).exists(): 
+            if User.objects.filter(email = email).exists():
                 return JsonResponse({"message": "EMAIL_IS_ALREADY_REGISTERED"}, status=400)
 
             #Insert Data
