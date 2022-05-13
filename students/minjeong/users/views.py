8debc17f9d5f997311c71ecf7cb186fe238249a5
minjeong kim <ebsgreat7@gmail.com>
2022-05-13 17:00:01 +0900

    회원가입 재재재재수정

--- students/minjeong/users/views.py
+++ students/minjeong/users/views.py
@@ -39,5 +39,33 @@ class SignupView(View):
         except KeyError :
                 return JsonResponse({"message" : "WRONG_FIELD_NAME"}, status=400)
 
-        except Exception as E :
-            return JsonResponse({"message" : E}, status=400)
+        except Exception as e :
+            return JsonResponse({"message" : e}, status=400)
+
+class LoginView(View):
+    def post(self, request):
+
+        data     = json.loads(request.body)
+        email    = data["email"]
+        password = data["password"]
+#         signups=Signup.objects.all()
+
+#         # if input_data["email"] not in request.body or input_data["password"] not in request.body:
+#         #     return JsonResponse( {"message": "KEY_ERROR"}, status=400)
+        
+#         # Signup.objects.get(email=input_data["email"]) == None:
+#         #     return JsonResponse({"message" : "SUCCESS"}, status=201)
+#         # 계정이나 패스워드 키가 전달되지 않았을 경우, {"message": "KEY_ERROR"}, status code 400 을 반환합니다.
+#         try:
+#             for signup in signups:
+#                 if input_data["email"] not in signup.email:
+#                     return JsonResponse({"message":"등록된 아이디가 없습니다. 올바른 아이디를 입력해주세요. "},status=400)
+#                 else:
+#                     if Signup.objects.get(email=input_data["email"]).password != input_data["password"]:
+#                         return JsonResponse( {"message": "해당하는 비밀번호가 존재하지 않습니다."}, status=400)
+#                     else:
+#                         return JsonResponse( {"message": "SUCCESS"}, status=201)
+#         except :
+#             return JsonResponse({"message":"계정이나 패스워드가 전달되지 않았습니다. "},status=400)
+
+
