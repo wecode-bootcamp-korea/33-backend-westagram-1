e4adb6ca17cd7f0b40f9d9154d7e297ee1fa53c6
strzero <restouri@gmail.com>
2022-05-15 13:22:05 +0900

    따옴표 수정

--- students/seyoung/users/views.py
+++ students/seyoung/users/views.py
@@ -7,10 +7,10 @@ from django.views import View
 
 from .models import User
 
-REGEX_EMAIL     = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-REGEX_PASSWORD  = '^(?=.{8,16}$)(?=.*[a-z])(?=.*[0-9]).*$'
-REGEX_MOBILE    = '\d{3}-\d{3,4}-\d{4}'
-REGEX_BIRTHDATE = '^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$'
+REGEX_EMAIL     = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+REGEX_PASSWORD  = "^(?=.{8,16}$)(?=.*[a-z])(?=.*[0-9]).*$"
+REGEX_MOBILE    = "\d{3}-\d{3,4}-\d{4}"
+REGEX_BIRTHDATE = "^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"
 
 class UserSignUpView(View):
     def post(self, request):
@@ -63,6 +63,6 @@ class UserSignInView(View):
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
         except JSONDecodeError:
-            return JsonResponse({'message':'JSON_DECODE_ERROR'}, status=400)
+            return JsonResponse({"message":"JSON_DECODE_ERROR"}, status=400)
 
         return JsonResponse({"message": "SUCCESS"}, status=200)
