7e23f00e26e9fc9a380fe164f595b5a3709147d2
jaeseung <jhwang90801@gmail.com>
2022-05-16 15:37:57 +0900

    cherry-pick project auth conflict fixed

--- students/jaeseung/users/views.py
+++ students/jaeseung/users/views.py
@@ -1,10 +1,13 @@
 import json
 import re
+import bcryt
+import jwt
 
 from django.http import JsonResponse
 from django.views import View
 
 from users.models import User
+from my_settings import SECRET_KEY, ALGORITHM
 
 class SignUpView(View):
     def post(self, request):
@@ -31,10 +34,26 @@ class SignUpView(View):
             User.objects.create(
                 name     = name,
                 email    = email,
-                password = password,
+                password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
                 contact  = contact
             )
             return JsonResponse({"message": "SUCCESS"}, status=201)
 
+        except KeyError: 
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+class SignInView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            email    = data['email']
+            password = data['password']
+
+            if not User.objects.filter(email = email, password = password).exists():
+                return JsonResponse({"message": "INVALID_USER"}, status=401)
+
+            return JsonResponse({"message": "SUCCESS"}, status=200)
+            
         except KeyError: 
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
