094e773d45dea46673fda577a2c0a6ae253895d3
minjeong kim <ebsgreat7@gmail.com>
2022-05-17 15:59:09 +0900

    재수정

--- students/minjeong/postings/models.py
+++ students/minjeong/postings/models.py
@@ -9,10 +9,9 @@ class Posting(models.Model):
     registered_date     = models.DateTimeField(auto_now_add=True)
     updated_at          = models.DateTimeField(auto_now=True)
     top_fixed           = models.BooleanField(default=False)
-    hits                = models.ManytoMany('signup.User', on_delete=models.CASCADE)
-# 1사람은 다양한 좋아요를 누를 수 있고 다양한 좋아요는 
+    hits                = models.OnetoOneField('signup.User', on_delete=models.CASCADE)
 
-
+# 로그인을 했다고 가정하고 
 class Comment(models.Model):
     posting_comment     = models.ForeignKey(Posting, on_delete=models.CASCADE)
     comment_content     = models.CharField(max_length=200, )
